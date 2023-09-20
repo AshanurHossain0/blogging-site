e95a03f167c9e6fd106b30b0cc6f74fb1b140c8b
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-09-20 14:00:18 +0530

    environment

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,4 +1,11 @@
 import mysql from 'mysql';
 
-const db=mysql.createConnection(JSON.parse(process.env.AUTH));
+const db = mysql.createConnection(
+    {
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    }
+);
 export default db;
